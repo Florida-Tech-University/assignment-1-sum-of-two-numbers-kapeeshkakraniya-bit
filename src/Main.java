3458010933cf71c25c6b460c949bb1d9c4ce21c9
kapeeshkakraniya-bit <kapeeshkakraniya@gmail.com>
2026-01-17 00:19:39 +0530

    Updated Assignment week 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x + y);
+  }
     public static void main(String[] args) {
-        int x = 7;
-        int y= 5;
-        System.output.println(x+y);
-}  
-public static void main(String[] args) {
-
-    scanner sc = new
-        Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    System.out.print(a+b);
-    sc close();
-}
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a + b);
+        sc.close();
+    }
