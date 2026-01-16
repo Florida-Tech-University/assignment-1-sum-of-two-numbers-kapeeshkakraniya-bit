4bb9ea96b1fb0ac73ad9b9da8e9152ad23f16375
kapeeshkakraniya-bit <kapeeshkakraniya@gmail.com>
2026-01-17 00:25:40 +0530

    Update 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 import java.util.Scanner;
 
-public class Main {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 3;
-    System.out.println(x + y);
-  }
+class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.print(a + b);
-        sc.close();
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt() + s.nextInt());
     }
+}
